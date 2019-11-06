2f315a80e5c1e66deb00c27a742e5cb493b84231
bharatpaliwal-169 <56474196+bharatpaliwal-169@users.noreply.github.com>
2019-11-06 09:34:15 +0530

    Update reverseNumber.c

--- reverseNumber.c
+++ reverseNumber.c
@@ -17,3 +17,15 @@ int main()
     printf("Reversed Number = %d", reversedNumber);
     return 0;
 }
+
+
+
+------------------------------------------------------PYTHON FILE----------------------------------------------------
+n = int(input())
+rev_num = 0
+while n != 0 : 
+    rem = n % 10
+    rev_num = rev_num * 10 + rem
+    n = n//10
+    
+print(rev_num)
